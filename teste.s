ea3d980a79ae4101a810d3c9b0bce504402e5a12
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2024-09-11 19:20:44 -0300

    .

--- teste.s
+++ teste.s
@@ -1,19 +1,43 @@
-push dword 29
+.data
+overflow_msg db 'Erro: Overflow na multiplicacao!', 0xA
+overflow_len equ $ - overflow_msg
+LABEL1 dd 2
+
+.bss
+LABEL2 resb 4
+LABEL3 resb 4
+LABEL4 resb 4
+
+.text
 call input
-LOAD 29
-DIV 28
-STORE 30
-MUL 28
-STORE 31
-LOAD 29
-SUB 31
-STORE 31
-push dword 31
+MOV EAX, LABEL2
+LABEL5: MOV EBX, LABEL1
+IDIV EBX 
+MOV LABEL3, EAX
+MOV EBX, LABEL1
+IMUL EBX
+ JO overflow_handler
+MOV LABEL4, EAX
+MOV EAX, LABEL2
+MOV EBX, LABEL4
+SUB EAX, EBX
+MOV LABEL4, EAX
 call output
 COPY 30 29
-LOAD 29
+MOV EAX, LABEL2
 JMPP 4
 STOP
-push dword 2
-push dword 0
-call s_input
+overflow_handler:
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, overflow_msg
+    MOV EDX, overflow_len
+    INT 0x80
+    MOV EAX, 1
+    MOV EBX, 1
+    INT 0x80
+end:
+    MOV EAX, 1
+    MOV EBX, 0
+    INT 0x80
+
--- tradutor.cpp
+++ tradutor.cpp
@@ -11,6 +11,25 @@ using namespace std;
 
 map<int, string> instructions = {{1, "ADD"}, {2, "SUB"}, {3, "MUL"}, {4, "DIV"}, {5, "JMP"}, {6, "JMPN"}, {7, "JMPP"}, {8, "JMPZ"}, {9, "COPY"}, {10, "LOAD"}, {11, "STORE"}, {12, "INPUT"}, {13, "OUTPUT"}, {14, "STOP"}, {15, "S_INPUT"}, {16, "S_OUTPUT"}};
 
+string data = ".data\n"
+"overflow_msg db 'Erro: Overflow na multiplicacao!', 0xA\n"
+"overflow_len equ $ - overflow_msg\n";
+
+string bss = ".bss\n";
+string final_text = ".text\n_start:\n";
+map<int, string> text;
+
+/* Endereço da label no assembly inventado, Nome da label  */
+map<int, string> labels_list = {};
+/* Endereço da label no ia-32, Nome da label */
+map<int, string> labels_list_end = {};
+
+/* Endereço de memória, Nome da variável */
+map<int, string> variable_list = {};
+
+int label_number = 1;
+int contador = 0;
+
 vector<int> numbers;
 
 void translator(string input_file_name, string output_file_name)
@@ -51,6 +70,119 @@ void translator(string input_file_name, string output_file_name)
 
     size_t index = 0;
 
+    /* APAGAR !!! */
+    while (index < numbers.size())
+    {
+      int current_number = numbers[index];
+      cout << instructions[current_number] << endl;
+      if (current_number == 14){
+        break;
+      }
+      switch (current_number)
+      {
+      case 0:
+        break;
+        
+      /* Copy */
+      case 9:
+        index += 2;
+        contador += 3;
+        break;
+
+      /* Stop */
+      case 14:
+      contador += 1;
+        break;
+
+      /* S_Input */
+      case 15:
+        index += 2;
+        contador += 3;
+        break;
+
+      /* S_Output */
+      case 16:
+        index += 2;
+        contador += 3;
+        break;
+
+      default:
+        index++;
+        contador += 2;
+        break;
+      }
+      index++;
+    }
+
+    index = 0;
+    
+    /* Encontra o STOP */
+     while (index < numbers.size())
+    {
+      int current_number = numbers[index];
+      if (current_number == 14){
+        break;
+      }
+      switch (current_number)
+      {
+      case 0:
+        break;
+        
+      /* Copy */
+      case 9:
+        index += 2;
+        break;
+
+      /* Stop */
+      case 14:
+        break;
+
+      /* S_Input */
+      case 15:
+        index += 2;
+        break;
+
+      /* S_Output */
+      case 16:
+        index += 2;
+        break;
+
+      default:
+        index++;
+        break;
+      }
+
+      index++;
+    }
+
+    index++;
+
+    /* Depois do STOP */
+    while (index < numbers.size())
+    {
+      int current_number = numbers[index];
+
+      variable_list[index] = "LABEL" + to_string(label_number);
+
+      if (current_number == 0){
+        bss += "LABEL" + to_string(label_number) + " resb 4\n"; /* 4 bytes de memória (32 bits) */
+      } else {
+        data += "LABEL" + to_string(label_number) + " dd " + to_string(current_number) + "\n";
+      }
+
+      index++;
+      label_number++;
+      contador++;
+    }
+
+    cout << "Lista de variaveis: " << endl;
+    for (auto it = variable_list.begin(); it != variable_list.end(); ++it) {
+        cout << to_string(it->first) + ": " + it->second << endl;
+    }
+
+    index = 0;
+    contador = 0;
+
     while (index < numbers.size())
     {
       int current_number = numbers[index];
@@ -59,61 +191,209 @@ void translator(string input_file_name, string output_file_name)
       {
       case 0:
         break;
-        
+
+      /* ADD */
+      case 1:
+        index++;
+        if (variable_list.find(numbers[index]) != variable_list.end()) {
+          text[contador] = "MOV EBX, [" + variable_list[numbers[index]] + "]\nADD EAX, EBX\n";
+        } else {
+          text[contador] = "MOV EBX, " + to_string(numbers[index]) + "\nADD EAX, EBX\n";
+        }
+        contador += 2;
+        break;
+
+      /* SUB */
+      case 2:
+        index++;
+        if (variable_list.find(numbers[index]) != variable_list.end()) {
+          text[contador] = "MOV EBX, [" + variable_list[numbers[index]] + "]\nSUB EAX, EBX\n";
+        } else {
+          text[contador] = "MOV EBX, " + to_string(numbers[index]) + "\nSUB EAX, EBX\n";
+        }
+        contador += 2;
+        break;
+
+      /* MUL */
+      case 3:
+        index++;
+        if (variable_list.find(numbers[index]) != variable_list.end()) {
+          text[contador] = "MOV EBX, [" + variable_list[numbers[index]] + "]\nIMUL EBX\nCMP IDX, 0\nJNE overflow_handler\n";
+        } else {
+          text[contador] = "MOV EBX, " + to_string(numbers[index]) + "\nIMUL EBX\nCMP IDX, 0\nJNE overflow_handler\n";
+        }
+        contador += 2;
+        break;
+      
+      /* DIV */
+      case 4:
+        index++;
+        if (variable_list.find(numbers[index]) != variable_list.end()) {
+          text[contador] = "MOV EBX, [" + variable_list[numbers[index]] + "]\nXOR EDX, EDX\nIDIV EBX \n";
+        } else {
+          text[contador] = "MOV EBX, " + to_string(numbers[index]) + "\nXOR EDX, EDX\nIDIV EBX \n";
+        }
+        contador += 2;
+        break;
+      
+      /* JMP */
+      case 5:
+        index++;
+        if (labels_list.find(numbers[index]) != labels_list.end()) {
+          text[contador] = "JMP " + labels_list[numbers[index]] + "\n";
+        } else {
+          text[contador] = "JMP LABEL" + to_string(label_number) + "\n";
+          labels_list[numbers[index]] = "LABEL" + to_string(label_number);
+          label_number++;
+        }
+        contador += 2;
+        break;
+      
+      /* JMPN */
+      case 6:
+        index++;
+        if (labels_list.find(numbers[index]) != labels_list.end()) {
+          text[contador] = "JB " + labels_list[numbers[index]] + "\n";
+        } else {
+          text[contador] = "JB LABEL" + to_string(label_number) + "\n";
+          labels_list[numbers[index]] = "LABEL" + to_string(label_number);
+          label_number++;
+        }
+        contador += 2;
+        break;
+      
+      /* JMPP */
+      case 7:
+        index++;
+        if (labels_list.find(numbers[index]) != labels_list.end()) {
+          text[contador] = "JA " + labels_list[numbers[index]] + "\n";
+        } else {
+          text[contador] = "JA LABEL" + to_string(label_number) + "\n";
+          labels_list[numbers[index]] = "LABEL" + to_string(label_number);
+          label_number++;
+        }
+        contador += 2;
+        break;
+
+      /* JMPZ */
+      case 8:
+        index++;
+        if (labels_list.find(numbers[index]) != labels_list.end()) {
+          text[contador] = "JE " + labels_list[numbers[index]] + "\n";
+        } else {
+          text[contador] = "JE LABEL" + to_string(label_number) + "\n";
+          labels_list[numbers[index]] = "LABEL" + to_string(label_number);
+          label_number++;
+        }
+        contador += 2;
+        break;
+
       /* Copy */
       case 9:
         index++;
-        output_file << instructions[current_number] + " " + to_string(numbers[index]) + " ";
+        text[contador] = "MOV " + to_string(numbers[index+1]) + "," + to_string(numbers[index]) + "\n";
         index++;
-        output_file << to_string(numbers[index]) << endl;
+        contador += 3;
+        break;
+
+      /* LOAD */
+      case 10:
+        index++;
+        if (variable_list.find(numbers[index]) != variable_list.end()) {
+          text[contador] = "MOV EAX, " + variable_list[numbers[index]] + "\n";
+        } else {
+          text[contador] = "MOV EAX, " + to_string(numbers[index]) + "\n";
+        }
+        contador += 2;
+        break;
+
+      /* STORE */
+      case 11:
+        index++;
+        if (variable_list.find(numbers[index]) != variable_list.end()) {
+          text[contador] = "MOV " + variable_list[numbers[index]] + ", EAX\n";
+        } else {
+          text[contador] = "MOV " + to_string(numbers[index]) + ", EAX\n";
+        }
+        contador += 2;
         break;
 
       /* Input */
       case 12:
         index++;
-        output_file << "push dword " + to_string(numbers[index]) << endl;
-        output_file << "call input" << endl;
+        text[contador] = "call input\n";
+        contador += 2;
         break;
 
       /* Output */
       case 13:
         index++;
-        output_file << "push dword " + to_string(numbers[index]) << endl;
-        output_file << "call output" << endl;
+        text[contador] = "call output\n";
+        contador += 2;
         break;
 
       /* Stop */
       case 14:
-        output_file << instructions[current_number] << endl;
+        text[contador] = "JMP END\n";
+        contador += 1;
         break;
 
       /* S_Input */
       case 15:
         index++;
-        output_file << "push dword " + to_string(numbers[index]) << endl;
         index++;
-        output_file << "push dword " + to_string(numbers[index]) << endl;
-        output_file << "call s_input" << endl;
+        text[contador] = "call s_input\n";
+        contador += 3;
         break;
 
       /* S_Output */
       case 16:
         index++;
-        output_file << "push dword " + to_string(numbers[index]) << endl;
         index++;
-        output_file << "push dword " + to_string(numbers[index]) << endl;
-        output_file << "call s_output" << endl;
+        text[contador] = "call s_output\n";
+        contador += 3;
         break;
 
       default:
-        int extra_number_readed;
-        index++;
-        output_file << instructions[current_number] + " " + to_string(numbers[index]) << endl;
         break;
       }
 
       index++;
+      if (current_number == 14){
+        break;
+      }
     }
+
+    /* Adicionando as Labels no código */
+    for (auto it = labels_list.begin(); it != labels_list.end(); ++it) {
+        text[it->first] = it->second + ": " + text[it->first];
+    }
+
+    /* Adicionando as Labels no código */
+    for (auto it = text.begin(); it != text.end(); ++it) {
+        final_text += it->second;
+    }
+
+    final_text +=
+        "overflow_handler:\n"
+        "    MOV EAX, 4\n" /*sys_write */
+        "    MOV EBX, 1\n" /* stdout */
+        "    MOV ECX, overflow_msg\n" /* mensagem de erro */
+        "    MOV EDX, overflow_len\n" /* Comprimento da mensagem */
+        "    INT 0x80\n"
+        "    MOV EAX, 1\n" /* sys_exit */
+        "    MOV EBX, 1\n" /* Código de erro (1) */
+        "    INT 0x80\n";
+
+        final_text +=
+        "END:\n"
+        "    MOV EAX, 1\n" /* sys_exit */
+        "    MOV EBX, 0\n"
+        "    INT 0x80\n";
+
+    output_file << data << endl;
+    output_file << bss << endl;
+    output_file << final_text << endl;
 
     input_file.close();
     output_file.close();
